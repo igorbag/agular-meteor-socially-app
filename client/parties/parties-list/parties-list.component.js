3bc1e91bf8877e19ce01793161bd1bffcbc284aa
unknown <igorbag@hotmail.com>
2015-12-28 21:07:41 -0200

    Adicionando funcionalidades

---
+++ client/parties/parties-list/parties-list.component.js
@@ -0,0 +1,45 @@
+angular.module('socially').directive('partiesList', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'client/parties/parties-list/parties-list.html',
+        controllerAs: 'partiesList',
+        controller: function ($scope, $reactive) {
+            $reactive(this).attach($scope);
+
+            this.newParty = {};
+            this.perPage = 3;
+            this.page = 1;
+            this.sort = {
+                name: 1
+            };
+
+            this.subscribe('parties');
+
+            this.helpers({
+                    parties: () => {
+                    return Parties.find({}, { sort : this.getReactively('sort') });
+              }
+        });
+
+            this.subscribe('parties', () => {
+                return [
+                    {
+                        limit: parseInt(this.perPage),
+                        skip: parseInt((this.getReactively('page') - 1) * this.perPage),
+                        sort: this.getReactively('sort')
+                    }
+                ]
+            });
+
+            this.addParty = () => {
+                this.newParty.owner = Meteor.user()._id;
+                Parties.insert(this.newParty);
+                this.newParty = {};
+            };
+
+            this.removeParty = (party) => {
+                Parties.remove({_id: party._id});
+            }
+        }
+    }
+});
